e581e5b62892417e8f64ad57c7c365db19fe87a0
Dan Cashion <dangcashion@gmail.com>
2020-09-24 19:37:47 -0500

    Nav Bar

---
+++ clint/demolitionwebsite/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+      </Router>
+    
+  );
+}
+
+export default App;
